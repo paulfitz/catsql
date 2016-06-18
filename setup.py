c0ca1f7739c82ae1ab7b6a7bfc9fb47a6f288ff9
Paul Fitzpatrick <paul.michael.fitzpatrick@gmail.com>
2016-06-18 18:41:16 -0400

    0.2.11

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info[0] == 2:
     install_requires.append('unicodecsv')
 
 setup(name="catsql",
-      version="0.2.10",
+      version="0.2.11",
       author="Paul Fitzpatrick",
       author_email="paulfitz@alum.mit.edu",
       description="Display a quick view of sql databases (and make quick edits)",
